6905ca350d10a880c40b1a2ae8e810b0df507d67
Joseph Buckley <jbuckley213@gmail.com>
2021-02-18 01:38:50 +0100

    add build files

--- routes/stripe.router.js
+++ routes/stripe.router.js
@@ -166,16 +166,16 @@ router.post('/order/success', async (req, res) => {
 
 router.post('/confirmation-email', async (req,res, next)=>{
    
-//     const oauth2Client = new google.auth.OAuth2(
-//         process.env.CLIENT_ID_MAIL, // ClientID
-//         process.env.CLIENT_SECRET, // Client Secret
-//         "https://developers.google.com/oauthplayground" // Redirect URL
-//    );
-//    oauth2Client.setCredentials({
-//     refresh_token: process.env.REFRESH_TOKEN
-// });
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID_MAIL, // ClientID
+        process.env.CLIENT_SECRET, // Client Secret
+        "https://developers.google.com/oauthplayground" // Redirect URL
+   );
+   oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+});
 
-// const accessToken = await oauth2Client.getToken(code)
+const accessToken = await oauth2Client.getAccessToken()
 
     // console.log(accessToken)
     const {_id} = req.session.currentUser
@@ -202,20 +202,20 @@ router.post('/confirmation-email', async (req,res, next)=>{
 
 
  var transport = nodemailer.createTransport({
-    service:"gmail",
-    // host: 'smtp.gmail.com',
-    // port: 465,
-    // secure: true,
+    // service:"gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
  
-        // type: 'OAuth2',
+        type: 'OAuth2',
       user: "sportshub213@gmail.com",
        pass: process.env.EMAIL_PASSWORD,
-    //   clientId: process.env.CLIENT_ID_MAIL,
-    //   clientSecret: process.env.CLIENT_SECRET,
-    //   refreshToken: process.env.REFRESH_TOKEN,
+      clientId: process.env.CLIENT_ID_MAIL,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
     //   accessToken: process.env.ACCESS_TOKEN
-    // accessToken: accessToken,
+    accessToken: accessToken,
 
     }
   })
